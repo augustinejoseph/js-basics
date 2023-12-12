68893f11e605ee025681362eff4d3680adb16361
augustinejoseph <augustine.augustine4@gmail.com>
2023-12-12 19:52:31 +0530

    git element

--- getElement.js
+++ getElement.js
@@ -1,5 +1,25 @@
 const getElementFn = () => {
-    const firstName = document.getElementById("firstName");
-    console.log(firstName);
+    // get elements by id
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById('lastName')
+    const submitButton = document.getElementById('submitButton')
+    const resultsDiv = document.getElementById('getElementResultsDiv')
+
+    // get elements by classname
+    const resetButton = document.getElementsByClassName('resetButton')
+
+    // get Element by tagname
+    const formInputs = document.getElementsByTagName('input')
+
+    // Add event listener to show form input
+    submitButton.addEventListener('click', function () {
+        resultsDiv.innerHTML =
+            `
+        <p>First Name : ${firstNameInput.value}</p>
+        <p>Last Name : ${lastNameInput.value}</p>
+
+        `
+    })
+
 };
 document.addEventListener("DOMContentLoaded", getElementFn);
